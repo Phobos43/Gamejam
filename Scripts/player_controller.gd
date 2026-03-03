aa1db41433f8a4241b360a6a8200d6036c28e80a
Phobos43 <102238895+Phobos43@users.noreply.github.com>
2026-03-03 20:48:03 +0100

    Further updated player mouvement

--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -2,11 +2,14 @@ extends CharacterBody2D
 class_name Player
 
 const GROUND_ACCELERATION = 200.0
+const CROUCH_ACCELERATION = 80.0
 const GROUND_FRICTION = 0.8
 const AIR_SPEED = 200.0
 const JUMP_VELOCITY = -800.0
 const GRAVITY_MULTIPLIER = 2
 const CROUCH_SIZE = 0.5
+
+var is_crouched = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,16 +20,24 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-	if Input.is_action_pressed("crouch"):
-		scale.y = CROUCH_SIZE
+	is_crouched = Input.is_action_pressed("crouch")
+	
+	
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if is_crouched:
+		velocity.x += CROUCH_ACCELERATION * direction
+		scale.y = lerpf(scale.y, CROUCH_SIZE, 0.5)
 	else:
-		scale.y = 1
+		velocity.x += GROUND_ACCELERATION * direction
+		scale.y = lerpf(scale.y, 1, 0.5)
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
 	
-	velocity.x += GROUND_ACCELERATION * direction
+	
+	
 	velocity.x *= GROUND_FRICTION
 
 	move_and_slide()
