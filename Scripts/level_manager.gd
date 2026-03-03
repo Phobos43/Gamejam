a6fb42c942d5c3d1f8f34976258336546137ba6a
Phobos43 <102238895+Phobos43@users.noreply.github.com>
2026-03-03 18:47:14 +0100

    Second prototype
    New prototype with past shadow, started better player movement

--- Scripts/level_manager.gd
+++ Scripts/level_manager.gd
@@ -4,21 +4,45 @@ extends Node2D
 @onready var dead: TileMapLayer = $Dead
 @onready var player: CharacterBody2D = $Player
 @onready var level_end: Area2D = $Level_End
-@onready var label: Label = $Label
-@onready var poison: Area2D = $Poison
+@onready var shadow: CharacterBody2D = $Shadow
+@onready var door: StaticBody2D = $Door
+
+const player_start_pos = Vector2(90, 90)
 
 var is_dead = false
+var player_playback : Array[player_state] = []
+var is_playing_back = false
+var playback_frame = 0
+
+var time_left = 30
+var pressing_plate = false
+
+class player_state:
+	var position : Vector2
+	var door_pos : Vector2
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	dead.hide()
 	dead.collision_enabled = false
 	level_end.body_entered.connect(level_complete)
-	poison.body_entered.connect(player_died)
-
+	$Poison.body_entered.connect(player_died)
+	$Pressure_plate.body_entered.connect(pressed_plate)
+	$Pressure_plate.body_exited.connect(left_plate)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	time_left -= delta
+	$Timer.text = "Time left: %s" % round(time_left)
+	if not is_playing_back:
+		var new_state = player_state.new()
+		new_state.position = player.position
+		new_state.door_pos = door.position
+		player_playback.append(new_state)
+		
 	level_end.rotate(2*PI*delta)
+	
 	if is_dead == true:
 		alive.hide()
 		alive.collision_enabled = false
@@ -29,13 +53,47 @@ func _process(delta: float) -> void:
 		alive.collision_enabled = true
 		dead.hide()
 		dead.collision_enabled = false
-		
+	
+	if is_playing_back:
+		print(playback_frame)
+		if playback_frame < len(player_playback):
+			shadow.show()
+			shadow.position = player_playback[playback_frame].position
+			playback_frame += 1
+		else:
+			shadow.hide()
+			is_playing_back = false
+			print("stopped")
+	if is_playing_back:
+		if playback_frame < len(player_playback):
+			door.position = player_playback[playback_frame].door_pos
+	else:
+		if pressing_plate:
+			door.position.y = move_toward(door.position.y, -100, 5)
+		else:
+			door.position.y = move_toward(door.position.y, 100, 5)
 
 func player_died(body):
 	if body == player:
 		is_dead = true
+		is_playing_back = true
+		playback_frame = 0
+		player.global_position = player_start_pos
+
+func pressed_plate(body):
+	if body == player:
+		pressing_plate = true
+
+func left_plate(body):
+	if body == player:
+		pressing_plate = false
 
 func level_complete(body):
 	if body == player:
-		label.show()
+		$"Level victory".show()
 		player.hide()
+		is_playing_back = false
+
+
+func _on_button_pressed() -> void:
+	get_tree().reload_current_scene.call_deferred()
--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -1,26 +1,32 @@
 extends CharacterBody2D
 class_name Player
 
-const GROUND_SPEED = 400.0
+const GROUND_ACCELERATION = 200.0
+const GROUND_FRICTION = 0.8
 const AIR_SPEED = 200.0
-const JUMP_VELOCITY = -600.0
-
+const JUMP_VELOCITY = -800.0
+const GRAVITY_MULTIPLIER = 2
+const CROUCH_SIZE = 0.5
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * GRAVITY_MULTIPLIER * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_pressed("crouch"):
+		scale.y = CROUCH_SIZE
+	else:
+		scale.y = 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * GROUND_SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, GROUND_SPEED)
+	
+	velocity.x += GROUND_ACCELERATION * direction
+	velocity.x *= GROUND_FRICTION
 
 	move_and_slide()
