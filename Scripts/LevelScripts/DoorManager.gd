8d2b9151240c15a101e01e95b2e667cbb06982a1
Phobos43 <102238895+Phobos43@users.noreply.github.com>
2026-03-13 20:26:11 +0100

    Fixed texture components and implemented group indication

---
+++ Scripts/LevelScripts/DoorManager.gd
@@ -0,0 +1,48 @@
+@tool
+extends TextureComponent
+
+enum Easing {
+	Teleport,
+	Linear,
+	EaseOut
+}
+
+enum Positionset {
+	None,
+	InitialPos,
+	ToggledPos
+}
+
+@export var group_id_activation : int = 0
+@export var toggled : bool = false
+@export var easing_mode : Easing
+@export_group("Positions")
+@export var toggled_position : Vector2
+@export var initial_position := global_position
+@export var set_positions : Positionset = Positionset.None
+
+func _ready() -> void:
+	refresh($CollisionShape2D)
+
+func _physics_process(delta: float) -> void:
+	if set_positions == Positionset.None:
+		if toggled:
+			movement_to(toggled_position)
+		else:
+			movement_to(initial_position)
+	else:
+		if set_positions == Positionset.InitialPos:
+			initial_position = global_position
+		elif set_positions == Positionset.ToggledPos:
+			toggled_position = global_position
+			
+			
+func movement_to(target_pos: Vector2):
+	if easing_mode == Easing.Teleport:
+		global_position = target_pos
+	elif easing_mode == Easing.Linear:
+		global_position.x = move_toward(global_position.x, target_pos.x, 4)
+		global_position.y = move_toward(global_position.y, target_pos.y, 4)
+	elif easing_mode == Easing.EaseOut:
+		global_position.x = lerpf(global_position.x, target_pos.x, 0.1)
+		global_position.y = lerpf(global_position.y, target_pos.y, 0.1)
--- Scripts/LevelScripts/PlateManager.gd
+++ Scripts/LevelScripts/PlateManager.gd
@@ -7,7 +7,7 @@ signal toggle_group(id, length)
 @export var show_affecting_groups : bool = false
 
 func _ready() -> void:
-	collision_node = $Area2D/CollisionShape2D
+	refresh($Area2D/CollisionShape2D)
 	$Area2D.body_entered.connect(body_entered)
 
 var display_list = []
@@ -15,8 +15,15 @@ var display_list = []
 func _process(_delta: float) -> void:
 	if Engine.is_editor_hint():
 		if show_affecting_groups:
-			print(toggle_group.get_connections())
-
+			for node in get_tree().get_nodes_in_group("Dynamic Components"):
+				if node.group_id_activation == group_id_activation:
+					var indicator = Line2D.new()
+					indicator.points = [Vector2(0,0), node.position]
+					indicator.add_to_group("Group indicators " + str(group_id_activation))
+					add_child(indicator)
+		else:
+			for node in get_tree().get_nodes_in_group("Group indicator " + str(group_id_activation)):
+				node.queue_free()
 func body_entered(body:Node2D):
 	toggle_group.emit(group_id_activation, -1)
 	
