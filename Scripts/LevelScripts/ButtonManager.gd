e92d7e9e0f7ee89b21a02f1eacc7b6c061e43095
Phobos43 <102238895+Phobos43@users.noreply.github.com>
2026-03-15 09:43:55 +0100

    Started implementing texture buttons

--- Scripts/LevelScripts/ButtonManager.gd
+++ Scripts/LevelScripts/ButtonManager.gd
@@ -3,11 +3,11 @@ extends TextureComponent
 
 @export var group_id_activation : int = 0
 @export var show_affecting_groups : bool = false
+@export var activation_time : float = 1.0
+var timer : float
 
 func _ready() -> void:
 	refresh($Area2D/CollisionShape2D)
-	$Area2D.body_entered.connect(body_entered)
-	$Area2D.body_exited.connect(body_exited)
 
 var display_list = []
 
@@ -23,16 +23,17 @@ func _process(_delta: float) -> void:
 		else:
 			for node in get_tree().get_nodes_in_group("Group indicators " + str(group_id_activation)):
 				node.queue_free()
+	for node in $Area2D.get_overlapping_bodies():
+		if node is Player and Input.is_action_just_pressed("interact"):
+			
 
-func body_entered(body:Node2D):
-	if body is not Player: return
+func turn_on():
 	for node in get_tree().get_nodes_in_group("Dynamic Components"):
 		if node.group_id_activation == group_id_activation:
 			node.toggled = true
 	
 
-func body_exited(body:Node2D):
-	if body is not Player: return
+func turn_off():
 	for node in get_tree().get_nodes_in_group("Dynamic Components"):
 		if node.group_id_activation == group_id_activation:
 			node.toggled = false
